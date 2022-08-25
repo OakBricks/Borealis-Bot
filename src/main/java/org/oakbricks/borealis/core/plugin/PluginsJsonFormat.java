5998e26a8aafdaa9715f1b75dc913cc5013c5674
OakBricks <andycruzfranco@gmail.com>
2022-08-25 16:19:26 -0500

    remove comment and constructor bullshit

--- src/main/java/org/oakbricks/borealis/core/plugin/PluginsJsonFormat.java
+++ src/main/java/org/oakbricks/borealis/core/plugin/PluginsJsonFormat.java
@@ -18,9 +18,4 @@ public class PluginsJsonFormat {
     String mainClass;
     String[] authors;
     String[] dependencies;
-    /**
-     * This tells if a mod is "secure"
-     * If a mod is secure it doesn't have access to files, cant use mixin, ASM, or similar. (And unless <code>treatUnsecureAsUnsecure</code> is disabled, the bot will throw a warning listing unsecure mods)
-     * Secure mods are recommended because they limit the possibilities of plugins that could be hijacked for use in raiding, DM spamming, and token-grabbing
-     */
 }
